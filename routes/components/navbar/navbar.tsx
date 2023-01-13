584480730c819dfdafadc250757d28636b8d95c5
mchtsz <mhatland206@gmail.com>
2023-01-13 11:13:34 +0100

    1

---
+++ routes/components/navbar/navbar.tsx
@@ -0,0 +1,28 @@
+import { useClient } from "@klevn/jxc";
+import style from "./navbar.module.css"
+
+const openNav = () => {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+const closeNav = () => {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+useClient({openNav, closeNav})
+
+export default function Navbar() {
+    return <nav>
+        <div id="myNav" class={style.overlay}>
+            <a href="javascript:void(0)" class={style.closebtn} onclick={closeNav}>&times;</a>
+            <div class={style.overlay_content}>
+                <a href="#">Arbeid</a>
+                <a href="#">Om meg</a>
+                <a href="#">Kontakt</a>
+            </div>
+        </div>
+        <div class={[style.meny, style.nav]}>
+            <span onclick={openNav}>Meny</span>
+        </div>
+    </nav>
+}
--- routes/index/index.tsx
+++ routes/index/index.tsx
@@ -1,5 +1,7 @@
 import "./style.css";
 
+import Navbar from "../components/navbar/navbar";
+
 export default () => {
     return <html lang="en">
 
@@ -7,36 +9,35 @@ export default () => {
         <meta charset="UTF-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet"/>
         <title>Nettside</title>
     </head>
     
     <body>
         <div class="all">
+            <Navbar></Navbar>
+
             <main>
-                <nav>
-                    <div class="nav" id="nav">
-
-                    </div>
-                </nav>
-
                 <section>
                     <div class="section" id="section">
-                        <h1>Wow</h1>
+                        
                     </div>
                 </section>
 
                 <article>
                     <div class="article" id="article">
-
+                        
                     </div>
                 </article>
-
-                <footer>
-                    <div class="footer" id="footer">
-
-                    </div>
-                </footer>
             </main>
+
+            <footer>
+                <div class="footer" id="footer">
+                    <p>&copy;2023 Marthin Hatland</p>
+                </div>
+            </footer>
         </div>
     </body>
     
