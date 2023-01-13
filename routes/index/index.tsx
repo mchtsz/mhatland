a2caf845004d4f9240e688a697d47557d4918855
mchtsz <mhatland206@gmail.com>
2023-01-13 08:39:46 +0100

    git init

---
+++ routes/index/index.tsx
@@ -0,0 +1,44 @@
+import "./style.css";
+
+export default () => {
+    return <html lang="en">
+
+    <head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Nettside</title>
+    </head>
+    
+    <body>
+        <div class="all">
+            <main>
+                <nav>
+                    <div class="nav" id="nav">
+
+                    </div>
+                </nav>
+
+                <section>
+                    <div class="section" id="section">
+                        <h1>Wow</h1>
+                    </div>
+                </section>
+
+                <article>
+                    <div class="article" id="article">
+
+                    </div>
+                </article>
+
+                <footer>
+                    <div class="footer" id="footer">
+
+                    </div>
+                </footer>
+            </main>
+        </div>
+    </body>
+    
+    </html>
+}
